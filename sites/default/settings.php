5d59168d4ceca831f7a8e1f75dd0709d2158d16f
steve smietana & alex brown <student@epicodus.com>
2015-10-21 11:14:13 -0700

    add title_case module

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'bookstore',
       'username' => 'admin',
       'password' => 'password',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
